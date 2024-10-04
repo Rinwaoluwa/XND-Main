5b660ee1f9202bfd2a7c696445dbf5d7c8fba2c5
Rinwaoluwa <117735533+Rinwaoluwa@users.noreply.github.com>
2024-10-04 15:27:09 +0100

    Add floating imag animation to XND-App Route.'

--- src/app/xnd-app/page.tsx
+++ src/app/xnd-app/page.tsx
@@ -1,5 +1,7 @@
+"use client"
+
 import React from 'react';
-import { AnimatePresence, motion, useCycle } from "framer-motion";
+import { animate, AnimatePresence, motion, useCycle } from "framer-motion";
 import Image from 'next/image';
 import Cta from '@/components/xnd/cta';
 import { apartContent } from '@/fixtures/apart';
@@ -7,33 +9,52 @@ import Link from 'next/link';
 import Faqs from '@/components/faqs';
 import Footer from '@/components/footer';
 
+
 export default function XndApp() {
     return (
         <div
             aria-describedby="XnD Web App"
         >
             <section
-                className='relative -z-20 container-padding-x bg-black pt-12 landing-page-hero-image w-full flex flex-col items-center gap-8'
+                className='relative -z-30 container-padding-x bg-black pt-12 landing-page-hero-image w-full'
                 aria-labelledby='hero'
                 aria-describedby='hero section'
             >
+                <motion.img src="/assets/images/laptop-blur.png" className='absolute -z-20 top-44 left-4 lg:left-28'
+                    animate={{ y: [0, -20, 0] }}
+                    transition={{
+                        ease: "easeInOut",
+                        duration: 3,
+                        repeat: Infinity,
+                    }}
+                ></motion.img>
+                <motion.img src="/assets/images/trade-status.png" className='absolute -z-20 top-48 lg:top-2/4 right-0 lg:right-40'
+                    animate={{ y: [0, -20, 0] }}
+                    transition={{
+                        ease: "easeInOut",
+                        duration: 3,
+                        repeat: Infinity,
+                    }}
+                ></motion.img>
                 <Image src="/assets/icons/ellipse3.svg" alt="Ellipse" className='absolute -z-10 top-0 right-0 rotate-180' width={200} height={200} />
                 <Image src="/assets/icons/ellipse3.svg" alt="Ellipse" className='absolute -z-10 bottom-0 left-0' width={200} height={200} />
-                <h1 className="text-white text-center text-4xl md:text-6xl font-[600]">
-                    Welcome to Exchange Next Door
-                </h1>
-                <p className="text-white text-center opacity-7">
-                    A platform where you can securely trade your unused gift cards for cryptocurrency,
-                    <br />
-                    offering a seamless and secure transaction experience.
-                </p>
-                <Link
-                    href="/login"
-                    className="py-2 px-4 w-fit lg:w-40 bg-[#FBBD08] lg:w-32 h-10 text-center hover:bg-opacity-90 rounded-3xl"
-                >
-                    Get started
-                </Link>
-                <Image src="/assets/images/laptop.png" alt='XND Desktop Previwe' width={892} height={601} />
+                <div aria-describedby='CTA' className='w-full flex flex-col items-center gap-8'>
+                    <h1 className="text-white text-center text-4xl md:text-6xl font-[600]">
+                        Welcome to Exchange Next Door
+                    </h1>
+                    <p className="text-[#5F5F5F] text-center opacity-7">
+                        A platform where you can securely trade your unused gift cards for cryptocurrency,
+                        <br />
+                        offering a seamless and secure transaction experience.
+                    </p>
+                    <Link
+                        href="/login"
+                        className="py-2 px-4 w-fit lg:w-40 bg-[#FBBD08] lg:w-32 h-10 text-center hover:opacity-20 rounded-3xl"
+                    >
+                        Get started
+                    </Link>
+                    <Image src="/assets/images/laptop.png" alt='XND Desktop Previwe' width={892} height={601} />
+                </div>
             </section>
             {apartContent.map((content, index) => (
                 <Cta
